c514d1a43712398399d884e65f2b4f83de1c5e54
Jamie Seward <jamie.d.seward@gmail.com>
2015-11-05 01:19:31 -0800

    added rgb to hsv and back

--- src/solar/rendering/color.cpp
+++ src/solar/rendering/color.cpp
@@ -63,6 +63,99 @@ namespace solar {
 		return static_cast<uint8_t>(clamp(0.f, 1.f, v) * 255.0f + 0.5f);
 	}
 
+	std::array<float, 3> color::to_hsv() const {
+		float c_max = std::max(std::max(_r, _g), _b);
+		float c_min = std::min(std::min(_r, _g), _b);
+		float c_delta = c_max - c_min;
+			
+		float h = 0.f;
+		float s = 0.f;
+		float v = 0.f;
+
+		if (c_delta > 0.f) {
+			if (c_max == _r) {
+				h = 60.f * (std::fmodf(((_g - _b) / c_delta), 6.f));
+			}
+			else if (c_max == _g) {
+				h = 60.f * (((_b - _r) / c_delta) + 2.f);
+			}
+			else if (c_max == _b) {
+				h = 60.f * (((_r - _g) / c_delta) + 4.f);
+			}
+			else {
+				ASSERT(false);
+			}
+
+			if (h < 0.f) {
+				h += 360.f;
+			}
+				
+			if (c_max > 0.f) {
+				s = c_delta / c_max;
+			}
+		}
+
+		v = c_max;
+
+		return{ h, s, v };
+	}
+
+	color make_color_from_hsv(std::array<float, 3>& hsv) {
+		return make_color_from_hsv(hsv[0], hsv[1], hsv[2]);
+	}
+
+	color make_color_from_hsv(float h, float s, float v) {
+		ASSERT(is_within_inclusive(0.f, 360.f, h));
+		ASSERT(is_within_inclusive(0.f, 1.f, s));
+		ASSERT(is_within_inclusive(0.f, 1.f, v));
+
+		float c = v * s;
+		float h_prime = std::fmodf(h / 60.f, 6.f);
+		float x = c * (1.f - std::fabs(std::fmodf(h_prime, 2.f) - 1.f));
+		float m = v - c;
+
+		float r = 0.f;
+		float g = 0.f;
+		float b = 0.f;
+
+		if (0.f <= h_prime && h_prime < 1.f) {
+			r = c;
+			g = x;
+			b = 0.f;
+		}
+		else if (1.f <= h_prime && h_prime < 2.f) {
+			r = x;
+			g = c;
+			b = 0.f;
+		}
+		else if (2.f <= h_prime && h_prime < 3.f) {
+			r = 0.f;
+			g = c;
+			b = x;
+		}
+		else if (3.f <= h_prime && h_prime < 4.f) {
+			r = 0;
+			g = x;
+			b = c;
+		}
+		else if (4.f <= h_prime && h_prime < 5.f) {
+			r = x;
+			g = 0.f;
+			b = c;
+		}
+		else if (5.f <= h_prime && h_prime < 6.f) {
+			r = c;
+			g = 0.f;
+			b = x;
+		}
+
+		r += m;
+		g += m;
+		b += m;
+
+		return color(r, g, b);
+	}
+
 	std::string color::to_string() const {
 		std::string s;
 		s += hex_value_to_string(convert_to_byte(_a));
--- src/solar/rendering/color.h
+++ src/solar/rendering/color.h
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <string>
+#include <array>
 
 namespace solar {
 
@@ -26,6 +27,7 @@ namespace solar {
 
 		uint32_t to_argb32() const;
 		std::string to_string() const;
+		std::array<float, 3> to_hsv() const;
 
 	private:
 		uint8_t convert_to_byte(float v) const;
@@ -34,6 +36,8 @@ namespace solar {
 		friend bool try_make_color_from_string(color& c, const char* s);
 		friend color make_color_from_string(const char* s);
 		friend color make_color_from_argb(uint32_t argb);
+		friend color make_color_from_hsv(std::array<float, 3>& hsv);
+		friend color make_color_from_hsv(float h, float s, float v);
 		friend color lerp(const color& c0, const color& c1, float t);
 	};
 
